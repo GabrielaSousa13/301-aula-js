6532d4ea642972a84522764e81e0a5bf53ce1f4e
GabrielaSousa <gabriela.sousa1713@gmail.com>
2018-10-31 21:59:39 -0300

    aula dois

---
+++ script.js
@@ -0,0 +1,35 @@
+// let texto = "texto";
+// let numero = 1;
+// let booleano = true;
+
+// let nome = prompt('Qual seu nome?');
+// let valor = prompt('Qual o valor do seu empréstimo?');
+
+// alert(`Olá ${nome}`);
+// alert(`Você gostaria de emprestar R$${valor}, certo?`);
+
+// let pretendePagar =  confirm('Você pretende pagar seu empréstimo?');
+
+// alert(`Ok, Obrigado por emprestar.`);
+
+let lista = [1, 2, 3, 4, 5];
+let objeto = {
+
+};
+
+let joaozinho ={
+    nome: 'Joao',
+    idade: 13,
+    namorada: undefined,
+    gatos: [
+        {
+            nome: 'Gatito',
+            raça: 'SRD',
+        },
+
+        {
+          nome: 'Gatunos',
+          raça: 'persa',  
+        }
+    ]
+}
